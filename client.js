1fea95bbcfc356f93bde0905de6a9eecefc765ac
UltimaChaos <74316923+RedMatrix1@users.noreply.github.com>
2023-01-01 16:44:10 +0900

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require('net');
 const client = new net.Socket();
 client.connect(65000, '127.0.0.1', () => {
   console.log('Connected');
-  client.write('Server & Client by UltimaChaos');
+  client.write('Code by UltimaChaos');
 });
 
 client.on('close', () => {
